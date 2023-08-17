0a5ffb7770c002d31b1143313d4cff1386a6d2e2
Vladyslav Gusiev <vladyslav.gusiev@gmail.com>
2023-08-17 13:22:38 +0200

    general layout is done, resizing is incorrect

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,20 @@ import Footer from "./Footer";
 function App() {
   return (
     <div className="App">
-        <NavBar />
-        <Title/>
-        <SearchBar/>
-        <LowerBody/>
-        <Footer/>
+        <div className="full-width">
+            <NavBar />
+        </div>
+
+        <div className="full-width">
+            <Title/>
+            <SearchBar/>
+            <LowerBody/>
+        </div>
+        <div className="full-width"></div>
+
+        <div className="full-width">
+            <Footer/>
+        </div>
     </div>
   );
 }
